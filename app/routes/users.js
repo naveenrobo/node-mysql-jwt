37c5616b557575db5fb2a24ac4fca95702178673
Naveen <naveenrobotics@live.in>
2018-12-18 16:15:03 +0530

    removed validation due to some issues

--- app/routes/users.js
+++ app/routes/users.js
@@ -3,6 +3,7 @@ const validate = require('express-validation');
 const {
     handleExceptions
 } = require('../middlewares/errorHandlers');
+const validator = require('./validation/users');
 
 router.post('/login', handleExceptions(require('../controllers/login').login));
 router.post('/signup', handleExceptions(require('../controllers/login').signUp));
